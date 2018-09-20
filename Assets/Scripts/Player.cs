ced3403cf3c828e707b34d91688499d202f13d73
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-09-20 15:18:24 -0500

    Player Movement and obstruction ID

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,26 +13,59 @@ using UnityEngine;
 //  + Resume moving once the monster has been defeated
 // - Deal elementally-modified damage upon input from a player
 public class Player : MonoBehaviour {
+
     //Fields
+    private Rigidbody2D playerRigidBody;
 
     //Serialized Fields
+    [SerializeField]
+    private float playerAttackSpeed;
     [SerializeField]
     private float baseDamageAmount;
     [SerializeField]
     private float damageModifier;
+    [SerializeField]
+    private float playerMovementSpeed;
+
     //Variables
+    private bool enemyPresent;
 
     // Use this for initialization
     void Start ()
     {
-		
+        playerRigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        Walk();
 	}
+
+    //Checks to see if there's a monster within range
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (collision.CompareTag("Enemy"))
+        {
+            enemyPresent = true;
+        }
+    }
+
+    //If the monster disappears, resets whether an enemy is present
+
+    //Walks if unobstructed, stops if obstructed
+    private void Walk()
+    {
+        if (!enemyPresent)
+        {
+            playerRigidBody.velocity = new Vector2(playerMovementSpeed, 0);
+        }
+        else
+        {
+            playerRigidBody.velocity = new Vector2(0,0);
+        }
+    }
 
     //Attacks when an enemy is in range
     void Attack()
