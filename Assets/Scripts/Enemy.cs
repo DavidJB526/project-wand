30e81dca2c02ce7c19f2f7a6e83bd0c5a7158737
DavidJB526 <david.beaudoin@loop.colum.edu>
2018-09-20 13:56:35 -0500

    Add Enemy and Projectile Scripts and Prefabs

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    float health, goldAmount;
+
+    enum Weakness { Fire, Plant, Water };
+
+}
