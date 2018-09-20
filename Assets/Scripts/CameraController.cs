f5c164ae1fb406887974938501660a7d03e5bb68
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-09-20 11:18:42 -0500

    Project QoL Update + Script Beginnings
    Added folder hierarchy for asset integration, added scene for script testing, added script bases for movement and camera control, learned from some mistakes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Maintains camera focus on the player (with editor-accessible offset)
+// ((Possible feature)) Can de-parent and then refocus when transitioning between levels
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Necessary functions of the player:
+// - Acquire gold
+// - Deal Damage to monsters
+// - Modify said damage with an adjustable multiplier (for items)
+// - Move
+//  + Stop moving in the presence of a monster 
+//      (forward trigger zone can be used for both this and to acquire the monster object for damage-dealing)
+//  + Resume moving once the monster has been defeated
+// - Deal elementally-modified damage upon input from a player
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
