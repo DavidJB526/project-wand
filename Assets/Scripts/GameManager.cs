5978ad4af206b5068bd424b50ed7c48a273c22f4
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-10-25 15:15:20 -0500

    End of Class

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    //Things the Game Manager should do:
+    // - Keep track of Gold
+    // - Keep track of Level
+    // - These things be done elsewhere? I'm not sure
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
