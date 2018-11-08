bc5a0182639ead44e3f45d9bbad009e1ff1732e3
RSand18 <43037672+RSand18@users.noreply.github.com>
2018-11-08 10:55:20 -0600

    Added Inventory Script
    After nine years and two crashes in development, hopefully it will have been worth the wait.

---
+++ Assets/Scripts/InventoryController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour {
+
+    //Declare your item types
+
+    enum ItemTypes { Potions, Spellbooks }
+
+    //Make an array to hold your count for each item
+
+    int[] items = new int[2];
+
+    /*void AddItem(ItemType itemType, int Count);
+    void RemoveItem(ItemType itemType, int Count);
+    int CheckItemCount(ItemType itemType, int Count);
+    bool InventorySystem.CheckIfWeHaveItem(ItemType itemType, int Count);
+    ItemType[] TellMeItemsThatIHaveAtLeastOneOf();
+    */
+
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
