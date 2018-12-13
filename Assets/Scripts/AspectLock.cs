1870be6226495a8ed05e076a5c1bc3d289bd9e72
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-12-12 18:53:14 -0600

    Stitched together the game

---
+++ Assets/Scripts/AspectLock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectLock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    private void Awake()
+    {
+        //Set screen size for Standalone
+#if UNITY_STANDALONE
+        Screen.SetResolution(500, 800, false);
+        Screen.fullScreen = false;
+#endif
+    }
+}
